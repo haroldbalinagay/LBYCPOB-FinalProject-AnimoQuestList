e4edb2615678e17b76551d0866d1e38e58a2e9b3
Timothy Sze <timothytimothysze@gmail.com>
2026-08-09 17:22:47 +0800

    feat: Created Admin & Student classes with only schema attributes

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/Admin.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/Admin.java
@@ -1,4 +1,17 @@
 package ph.edu.dlsu.lbycpob.animoquest.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue("ADMIN")
 public class Admin extends User {
+    // No schema attributes
+
+    // TODO: Add admin attributes & behaviors
 }
--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/Student.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/Student.java
@@ -1,4 +1,18 @@
 package ph.edu.dlsu.lbycpob.animoquest.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue("STUDENT")
 public class Student extends User {
+    @Column(nullable = false)
+    private String major;
+
+    // TODO: Add student attributes & behaviors
 }
