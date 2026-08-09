685c9debe7efad6a3df8fc1f7b29b90babcd563b
Timothy Sze <timothytimothysze@gmail.com>
2026-08-09 17:16:01 +0800

    feat: Created User base class with only schema attributes

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/User.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/model/User.java
@@ -1,4 +1,37 @@
 package ph.edu.dlsu.lbycpob.animoquest.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
 public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // "insertable=false, updatable=false" since user_type is used as a discriminator (hibernate auto inserts value)
+    @Column(name = "user_type", nullable = false, insertable=false, updatable=false)
+    private String type;
+
+    @Column(name = "id_number", nullable = false)
+    private Long idNumber;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    // TODO: Add common attributes & behaviors for user
 }
