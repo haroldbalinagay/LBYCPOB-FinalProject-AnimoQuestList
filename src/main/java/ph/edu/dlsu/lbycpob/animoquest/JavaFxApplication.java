d50797009ee2e4c79c3048eeff4307bc1a3a55aa
Edwin Gerilla Jr <edwin_gerilla@dlsu.edu.ph>
2026-08-15 13:59:26 +0800

    Modified JavaFxApplication.java (replaced MyController with LoginController)

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/JavaFxApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/JavaFxApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import ph.edu.dlsu.lbycpob.animoquest.controller.LoginController;
 
 public class JavaFxApplication extends Application {
 
@@ -25,7 +26,7 @@ public class JavaFxApplication extends Application {
     @Override
     public void start(Stage stage) {
         FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
-        Parent root = fxWeaver.loadView(MyController.class);
+        Parent root = fxWeaver.loadView(LoginController.class);
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
