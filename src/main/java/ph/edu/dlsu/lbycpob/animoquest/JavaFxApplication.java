c8130eae4c11051830629cc68ecb926848d1f05d
Timothy Sze <timothytimothysze@gmail.com>
2026-08-16 17:16:21 +0800

    Replaced MyController with MainController in start() of JavaFxApplication

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/JavaFxApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/JavaFxApplication.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import ph.edu.dlsu.lbycpob.animoquest.controller.MainController;
+import ph.edu.dlsu.lbycpob.animoquest.controller.TermChecklistController;
 
 public class JavaFxApplication extends Application {
 
@@ -25,9 +27,10 @@ public class JavaFxApplication extends Application {
     @Override
     public void start(Stage stage) {
         FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
-        Parent root = fxWeaver.loadView(MyController.class);
+        Parent root = fxWeaver.loadView(MainController.class); // TODO: Likely merge conflict
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
