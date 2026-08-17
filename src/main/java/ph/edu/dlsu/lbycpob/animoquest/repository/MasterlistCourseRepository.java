971d7b001684996f4896defcc887b81c4fac8d19
Timothy Sze <timothytimothysze@gmail.com>
2026-08-17 17:08:08 +0800

    feat: Created 2 repositories for course_masterlist & term_checklists tables

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/MasterlistCourseRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/MasterlistCourseRepository.java
@@ -1,4 +1,19 @@
 package ph.edu.dlsu.lbycpob.animoquest.repository;
 
-public interface MasterlistCourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.animoquest.model.MasterlistCourse;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface MasterlistCourseRepository extends JpaRepository<MasterlistCourse, Long> {
+
+    Optional<MasterlistCourse> findMasterlistCourseById(Long id);
+
+    Optional<MasterlistCourse> findMasterlistCourseByNameIgnoreCase(String name);
+
+    // TODO: Doesn't actually find in order
+    List<MasterlistCourse> findByIdIn(long[] ids);
+
 }
--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/TermChecklistRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/TermChecklistRepository.java
@@ -1,4 +1,15 @@
 package ph.edu.dlsu.lbycpob.animoquest.repository;
 
-public interface TermChecklistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.animoquest.model.TermChecklist;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TermChecklistRepository extends JpaRepository<TermChecklist, Long> {
+
+    TermChecklist findTermChecklistByDegreeAndBatchAndTermNumber(String degree, int batch, int termNumber);
+
+    List<TermChecklist> findAllByDegreeAndBatch(String degree, int batch);
+
 }
