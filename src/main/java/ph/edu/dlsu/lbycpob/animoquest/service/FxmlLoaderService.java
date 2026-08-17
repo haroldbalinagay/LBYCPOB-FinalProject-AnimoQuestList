d8f47fd4e125f3276692d86f2f93338e401accfb
Timothy Sze <timothytimothysze@gmail.com>
2026-08-17 11:57:36 +0800

    feat: Created the FxmlLoaderService

---
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/FxmlLoaderService.java
@@ -0,0 +1,37 @@
+package ph.edu.dlsu.lbycpob.animoquest.service;
+
+import javafx.fxml.FXMLLoader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * FxmlLoaderService tells the JavaFX FXMLLoader to allow Spring to recognize the FXML file's controller as a managed Bean.
+ */
+@Service
+public class FxmlLoaderService {
+
+    private final ApplicationContext context;
+
+    // Spring automatically injects itself here
+    public FxmlLoaderService(ApplicationContext context) {
+        this.context = context;
+    }
+
+    /**
+     * Loads a FXML file and allows Spring to manage its controller as a Bean.
+     * @param fxmlPath
+     * @return The parent node of the FXML file
+     * @param <T>
+     * @throws IOException
+     */
+    public <T> T load(URL fxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(fxmlPath);
+
+        // Link the existing context to this specific loader instance
+        loader.setControllerFactory(context::getBean);
+
+        return loader.load();
+    }
+}
