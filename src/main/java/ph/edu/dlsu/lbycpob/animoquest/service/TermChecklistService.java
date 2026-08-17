891369c9b7a79a5e9106d07ec5b8124b557c263e
Timothy Sze <timothytimothysze@gmail.com>
2026-08-17 23:22:05 +0800

    feat: Created the TermChecklistService

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/TermChecklistService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/TermChecklistService.java
@@ -1,4 +1,34 @@
 package ph.edu.dlsu.lbycpob.animoquest.service;
 
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.animoquest.model.MasterlistCourse;
+import ph.edu.dlsu.lbycpob.animoquest.model.TermChecklist;
+import ph.edu.dlsu.lbycpob.animoquest.repository.MasterlistCourseRepository;
+import ph.edu.dlsu.lbycpob.animoquest.repository.TermChecklistRepository;
+
+import java.util.List;
+
+@Service
 public class TermChecklistService {
+    private final MasterlistCourseRepository courseRepository;
+    private final TermChecklistRepository checklistRepository;
+
+    public TermChecklistService(MasterlistCourseRepository courseRepository, TermChecklistRepository checklistRepository) {
+        this.courseRepository = courseRepository;
+        this.checklistRepository = checklistRepository;
+    }
+    
+    public List<TermChecklist> getTermChecklistsOf(String degree, int batch) {
+        if (degree == null || batch <= 0) return null;
+        return checklistRepository.findAllByDegreeAndBatch(degree, batch);
+    }
+
+    public TermChecklist getChecklistOf(String degree, int batch, int termNumber) {
+        if (degree == null || batch <= 0 || termNumber <= 0) return null;
+        return checklistRepository.findTermChecklistByDegreeAndBatchAndTermNumber(degree, batch, termNumber);
+    }
+
+    public List<MasterlistCourse> getCourseDetailsOf(long[] courseIds) {
+        return courseRepository.findByIdIn(courseIds);
+    }
 }
