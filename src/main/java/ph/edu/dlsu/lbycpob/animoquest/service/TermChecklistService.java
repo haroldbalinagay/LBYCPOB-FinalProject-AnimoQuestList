80ecf7912f5f47972188a4edc9add66bc3894a5f
Timothy Sze <timothytimothysze@gmail.com>
2026-08-18 18:29:30 +0800

    feat: Implemented searching for the status of a course in student progress records

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/TermChecklistService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/TermChecklistService.java
@@ -1,6 +1,8 @@
 package ph.edu.dlsu.lbycpob.animoquest.service;
 
 import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.animoquest.model.CourseStatus;
+import ph.edu.dlsu.lbycpob.animoquest.model.CurriculumProgress;
 import ph.edu.dlsu.lbycpob.animoquest.model.MasterlistCourse;
 import ph.edu.dlsu.lbycpob.animoquest.model.TermChecklist;
 import ph.edu.dlsu.lbycpob.animoquest.repository.MasterlistCourseRepository;
@@ -8,6 +10,7 @@ import ph.edu.dlsu.lbycpob.animoquest.repository.TermChecklistRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -15,9 +18,12 @@ public class TermChecklistService {
     private final MasterlistCourseRepository courseRepository;
     private final TermChecklistRepository checklistRepository;
 
-    public TermChecklistService(MasterlistCourseRepository courseRepository, TermChecklistRepository checklistRepository) {
+    private final CurriculumService progressService;
+
+    public TermChecklistService(MasterlistCourseRepository courseRepository, TermChecklistRepository checklistRepository, CurriculumService progressService) {
         this.courseRepository = courseRepository;
         this.checklistRepository = checklistRepository;
+        this.progressService = progressService;
     }
     
     public List<TermChecklist> getTermChecklistsOf(String degree, int batch) {
@@ -51,5 +57,34 @@ public class TermChecklistService {
             reqObject.ifPresent(masterlistCourse -> reqs.add(masterlistCourse.getCode()));
         }
         return reqs;
+    }
+
+    /**
+     * Searches for the MOST RECENT record containing the given course in the curriculum progress records of a student.
+     * @param course The course to look for
+     * @param progressList The complete curriculum progress records of a student
+     * @return The most recent status of the course
+     */
+    public CourseStatus getStatusOf(MasterlistCourse course, List<CurriculumProgress> progressList) {
+        CourseStatus status = CourseStatus.NOT_TAKEN;
+        int latestTermTaken = 0;
+
+        // Loop through each progress record of the student
+        // NO BREAK: Since possible to take the same course multiple times (ex. failed Term 1 -> take again Term 2)
+        for (CurriculumProgress progress : progressList) {
+            // Check if source course is the same as course in record
+            if (Objects.equals(course.getId(), progress.getCourseId())) {
+                // Save the status of the MOST RECENT info about course
+                if (progress.getTermTaken() >= latestTermTaken) {
+                    status = progress.getStatus();
+                }
+            }
+        }
+        IO.println(status);
+        return status;
+    }
+
+    public boolean checkEnrollEligibilityOf(MasterlistCourse course) {
+        return false;
     }
 }
