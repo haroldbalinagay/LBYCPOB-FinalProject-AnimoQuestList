57c6f1eca253ad3a1bac8c23c0e03dba99344dd2
Timothy Sze <timothytimothysze@gmail.com>
2026-08-18 18:28:20 +0800

    feat: Implemented searching for the status of a course in student progress records

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/CurriculumService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/service/CurriculumService.java
@@ -1,4 +1,22 @@
 package ph.edu.dlsu.lbycpob.animoquest.service;
 
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.animoquest.model.CurriculumProgress;
+import ph.edu.dlsu.lbycpob.animoquest.model.Student;
+import ph.edu.dlsu.lbycpob.animoquest.repository.CurriculumRepository;
+
+import java.util.List;
+
+@Service
 public class CurriculumService {
+    private final CurriculumRepository progressRepository;
+
+    public CurriculumService(CurriculumRepository progressRepository) {
+        this.progressRepository = progressRepository;
+    }
+
+    public List<CurriculumProgress> getProgressOf(Student student) {
+        Long temp = 1L;
+        return progressRepository.findByStudentId(temp);
+    }
 }
