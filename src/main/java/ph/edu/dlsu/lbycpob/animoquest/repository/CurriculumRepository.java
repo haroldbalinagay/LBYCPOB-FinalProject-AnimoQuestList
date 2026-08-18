0f7447d3a554fe78ff7788cf06d984ec124beeac
Edwin Gerilla Jr <edwin_gerilla@dlsu.edu.ph>
2026-08-19 07:47:29 +0800

    Modified CurriculumRepository.java

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/CurriculumRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/CurriculumRepository.java
@@ -1,4 +1,28 @@
 package ph.edu.dlsu.lbycpob.animoquest.repository;
 
-public interface CurriculumRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.animoquest.model.CurriculumProgress;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CurriculumRepository
+        extends JpaRepository<CurriculumProgress, Long> {
+
+    List<CurriculumProgress> findByStudentId(Long studentId);
+
+    List<CurriculumProgress> findByStudentIdAndTermTaken(
+            Long studentId,
+            int termTaken
+    );
+
+    Optional<CurriculumProgress> findByStudentIdAndCourseId(
+            Long studentId,
+            Long courseId
+    );
+
+    void deleteByStudentIdAndCourseId(
+            Long studentId,
+            Long courseId
+    );
 }
