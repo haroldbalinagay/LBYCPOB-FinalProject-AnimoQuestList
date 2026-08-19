f781da156d0b8cf88e31f6c4aa41512104593b1d
Edwin Gerilla Jr <edwin_gerilla@dlsu.edu.ph>
2026-08-19 11:15:24 +0800

    Modified MasterlistCourseRepository.java

--- src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/MasterlistCourseRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/animoquest/repository/MasterlistCourseRepository.java
@@ -3,7 +3,10 @@ package ph.edu.dlsu.lbycpob.animoquest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ph.edu.dlsu.lbycpob.animoquest.model.MasterlistCourse;
 
+import java.util.List;
+
 public interface MasterlistCourseRepository
         extends JpaRepository<MasterlistCourse, Long> {
 
+    List<MasterlistCourse> findByIdIn(List<Long> ids);
 }
